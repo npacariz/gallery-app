e3c482b7e1d66b13905147e84f2692280efc4ea5
npacariz <nikolapacariz@gmail.com>
2018-09-17 21:47:07 +0200

    added navbar to project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <nav class="navbar navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">Gallery App</a>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
