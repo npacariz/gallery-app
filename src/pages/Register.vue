32a0ca0761b02ae6bc1158639219b256427ff9ed
npacariz <nikolapacariz@gmail.com>
2018-09-18 15:42:20 +0200

    Added user register functionality

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,38 +1,69 @@
 <template>
     <div>
         <h1>Register</h1>
-        <form>
+        <form @submit.prevent="register">
             <div class="form-group">
                 <label for="first-name">First Name</label>
-                <input type="text" class="form-control" id="first-name" placeholder="First Name">
+                <input type="text" class="form-control" id="first-name" v-model="newUser.first_name">
+                <p class="alert alert-danger" v-if="errors.first_name">{{errors.first_name[0]}}</p>
             </div>
             <div class="form-group">
                 <label for="last-name">Last Name</label>
-                <input type="text" class="form-control" id="last-name">
+                <input type="text" class="form-control" id="last-name"  v-model="newUser.last_name">
+                 <p class="alert alert-danger" v-if="errors.last_name">{{errors.last_name[0]}}</p>
             </div>
              <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email">    
+                <input type="email" class="form-control" id="email" v-model="newUser.email"> 
+                 <p class="alert alert-danger" v-if="errors.email">{{errors.email[0]}}</p>   
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" id="password">
+                <input type="password" class="form-control" id="password" v-model="newUser.password">
+                 <p class="alert alert-danger" v-if="errors.password">{{errors.password[0]}}</p>
             </div>
              <div class="form-group">
                 <label for="password-confirmation">Confirmed Password</label>
-                <input type="password" class="form-control" id="password-confirmation">
+                <input type="password" class="form-control" id="password-confirmation" v-model="newUser.password_confirmation">
             </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="terms-and-conditions">
-                <label class="form-check-label" for="terms-and-conditions">Accepted terms and conditions</label>
+            <div class="form-group">
+               <input type="checkbox" class="form-check-input" v-model="newUser.terms_and_conditions">
+                <label class="form-check-label" for="exampleCheck1">Accepted terms and conditions</label> 
+                <p class="alert alert-danger" v-if="errors.terms_and_conditions">{{errors.terms_and_conditions[0]}}</p>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </template>
 <script>
+import { auth } from "./../services/AuthService.js";
 export default {
-  name: "Register"
+  name: "Register",
+  data() {
+    return {
+      newUser: {
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        terms_and_conditions: false
+      },
+      errors: {}
+    };
+  },
+  methods: {
+    register() {
+      auth
+        .register(this.newUser)
+        .then(() => {
+          this.$router.push({ name: "all-galleries" });
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
 };
 </script>
 <style scoped>
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -5,9 +5,9 @@ export default class AuthService {
     axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
     this.setAxiosDefaultAuthorizationHeader();
   }
-  /*
-  Login method, sending request for token to database
-  */
+  /**
+   *  Login method, sending request for token to database
+   */
   login(email, password) {
     return axios.post(`auth/login`, { email, password }).then(response => {
       const token = response.data.access_token;
@@ -15,20 +15,30 @@ export default class AuthService {
       this.setAxiosDefaultAuthorizationHeader();
     });
   }
-  /*
-  Setting a token to axios header for every request
-  */
+  /**
+   * Setting a token to axios header for every request
+   */
   setAxiosDefaultAuthorizationHeader() {
     const token = localStorage.getItem("token");
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   }
-  /*
-  Logout method, calling database to logout user;
-  */
+  /**
+   *  Logout method, calling database to logout user;
+   */
   logout() {
     return axios.post(`auth/logout`).then(() => {
       localStorage.removeItem("token");
       delete axios.defaults.headers.common["Authorization"];
+    });
+  }
+  /**
+   * Register method and automatically login user
+   */
+  register(newUser) {
+    return axios.post(`auth/register`, newUser).then(response => {
+      const token = response.data.access_token;
+      localStorage.setItem("token", token);
+      this.setAxiosDefaultAuthorizationHeader();
     });
   }
 }
